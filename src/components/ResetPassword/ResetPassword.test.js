9134a63bd28023b4c90a567dbc3d92d11c27c4d9
Gustavo Isensee <gustavo.isensee@gmail.com>
2019-04-14 17:30:08 +0200

    fix tests

--- src/components/ResetPassword/ResetPassword.test.js
+++ src/components/ResetPassword/ResetPassword.test.js
@@ -4,6 +4,15 @@ import ResetPassword from './ResetPassword';
 
 it('ResetPassword - renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<ResetPassword />, div);
+  ReactDOM.render(
+    <ResetPassword
+      values={{ password: 'test' }}
+      errors={{}}
+      handleBlur={() => {}}
+      handleChange={() => {}}
+      handleSubmit={() => {}}
+    />,
+    div
+  );
   ReactDOM.unmountComponentAtNode(div);
 });
