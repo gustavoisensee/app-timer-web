87064c5e942f122d3fe79a9fbc5ab030cb27aa80
Gustavo Isensee <gustavo.isensee@gmail.com>
2019-04-23 20:58:41 +0200

    add some style fixes

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -19,6 +19,32 @@ const months = [
 const data = [
   {
     year: 2019,
+    month: months[2],
+    income: [
+      3500, 133
+    ],
+    groups: [
+      {
+        name: 'Gloceries',
+        total: 300,
+        totalItems: 46.65,
+        items: [
+          { description: 'Albert heijn 1', value: 12.33 },
+          { description: 'Albert heijn 2', value: 34.22 },
+        ]
+      },
+      {
+        name: 'Health Ensurance',
+        total: 235,
+        totalItems: 135,
+        items: [
+          { description: 'Bru HE', value: 135 },
+        ]
+      }
+    ]
+  },
+  {
+    year: 2019,
     month: months[1],
     income: [
       3500, 133
