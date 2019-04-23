d595741c95844efa47a02e3d581d99abd204ff85
Gustavo Isensee <gustavo.isensee@gmail.com>
2019-04-23 06:59:17 +0200

    initial commit for the Dashboard, creating the structure of costs

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,62 @@
 import React from 'react';
+import './styles.scss';
 
-const Dashboard = () => (<div>Dashboard</div>);
+const data = [
+  {
+    year: 2019,
+    month: 1,
+    income: [
+      3500, 133
+    ],
+    groups: [
+      {
+        name: 'Gloceries',
+        total: 300,
+        totalItems: 46.65,
+        items: [
+          { description: 'Albert heijn 1', value: 12.33 },
+          { description: 'Albert heijn 2', value: 34.22 },
+        ]
+      }
+    ]
+  }
+];
+
+const Dashboard = () => (
+  <div>
+    <h2>Dashboard</h2>
+    {data && data.map((m, i) => (
+      <div key={`month-${i}`} className="Card">
+        <div>
+          <span>{m.year}</span>/
+          <span>{m.month}</span>
+        </div>
+        <div>
+          <span>{m.income.reduce((acc, cur) => (acc + cur), 0)}</span>
+        </div>
+        <div>
+          {m.groups && m.groups.map((g, i) => (
+            <div key={`group-${i}`}>
+              <span>{g.name}</span>&nbsp;
+              <span>{g.total}</span>
+              {g.items && g.items.map((item, i) => (
+                <div key={`item-${i}`}>
+                  <span>{item.description}</span>&nbsp;
+                  <span>{item.value}</span>
+                </div>
+              ))}
+              <div>
+                <span>Total spent: {g.totalItems}</span>
+              </div>
+              <div>
+                <span>Total left: {(g.total - g.totalItems)}</span>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    ))}
+  </div>
+);
 
 export default Dashboard;
