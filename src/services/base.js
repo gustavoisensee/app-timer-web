353e56b719a6e3241eff8c2b4cb0ede0c6944023
Gustavo Isensee <gustavo.isensee@gmail.com>
2021-04-02 08:48:34 +0200

    revert apiUrl change

--- src/services/base.js
+++ src/services/base.js
@@ -1,5 +1,5 @@
 const apiUrl = (
-  process.env.REACT_APP_PATH_API || 'http://127.0.0.1:4000/'
+  process.env.REACT_APP_PATH_API || 'https://my-finances-api-v1.herokuapp.com/'
 );
 
 const createUrl = (path) => `${apiUrl}${path}`;
